c810f333843ec54bd1a3ea59bc1fd39d882af796
Halanov Sergiy <dukalis.rak@gmail.com>
2016-04-26 00:27:29 +0300

    +SlowMo
    +box collider на terrain

---
+++ Assets/SlowMO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowMO : MonoBehaviour {
+    public float timeScale;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Time.timeScale = timeScale;
+	
+	}
+}
